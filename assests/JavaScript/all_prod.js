e36e18c9a0399a31cbaf6a95fabe670b40adc6ba
Pedro_V.S Ferreira <pedrovinicius.sf33@gmail.com>
2022-06-29 00:07:25 -0300

    zzz

---
+++ assests/JavaScript/all_prod.js
@@ -0,0 +1,159 @@
+const arquivo_produtos = [
+  //vestuario
+  {
+    codeID: "vestuario01",
+    nome: "Moletom Unissex Hoddie Marvel",
+    preco:"79,90",
+    imagem:"assests/imagens/moletom-marvel-frente.webp",
+  },
+  {
+    codeID: "vestuario02",
+    nome: "Blusa Moletom Unissex Batman",
+    preco:"79,90",
+    imagem:"assests/imagens/moletom-batman-frente.webp",
+  },
+  {
+    codeID: "vestuario03",
+    nome: "Camiseta Dupla Face the Boys Diabolical",
+    preco:"59,90",
+    imagem:"assests/imagens/camiseta-the-boys-frente.webp",
+  },
+  {
+    codeID: "vestuario03",
+    nome: "Camiseta Dupla Face Stranger Things",
+    preco:"59,90",
+    imagem:"assests/imagens/camiseta-stranger-things-frente.webp",
+  },
+
+  //almofadas
+
+  {
+    
+    codeID:"",codeID:"almofadas01",
+    nome: "'",
+    preco:"'",
+    imagem:"",
+  },
+  {
+    
+    codeID:"",codeID:"almofadas02",
+    nome: "'",
+    preco:"'",
+    imagem:"",
+  },
+  {
+    
+    codeID:"",codeID: "almofadas03",
+    nome: "'",
+    preco:"'",
+    imagem:"",
+  },
+  {
+    codeID: "almofadas04",
+    nome: "caneca super-vo",
+    preco:"'",
+    imagem:"",
+  },
+  //canecas
+  {
+    codeID: "caneca01",
+    nome: "Caneca Super-Vó",
+    preco:"79,90",
+    imagem:"assests/imagens/caneca-super-vo.jpg",
+  },
+  {
+    codeID:"caneca02",
+    nome: "Caneca Homem de Ferro",
+    preco:"79,90",
+    imagem:"",
+  },
+  {
+    codeID:"caneca03",
+    nome: "Caneca Homem Aranha",
+    preco:"59,90",
+    imagem:"",
+  },
+  {
+    codeID:"caneca04",
+    nome: "Caneca Naruto",
+    preco:"59,90",
+    imagem:"",
+  },
+
+  //quadros
+  {
+    codeID:"quadros01",
+    nome: "Quado Rick and Morty",
+    preco:"79,90",
+    imagem:"",
+  },
+  {
+    codeID:"quadro02",
+    nome: "Quadro Star Wars",
+    preco:"79,90",
+    imagem:"",
+  },
+  {
+    codeID:"quadro03",
+    nome: "Quadro Super Mario",
+    preco:"59,90",
+    imagem:"",
+  },
+  {
+    codeID:"quadro04",
+    nome: "Quadro The Flash",
+    preco:"59,90",
+    imagem:"",
+  },
+  //livros
+  {
+    codeID:"livro01",
+    nome: "Livro Game of Thrones",
+    preco:"79,90",
+    imagem:"",
+  },
+  {
+    codeID:"livro02",
+    nome: "Livro O Nome do Vento",
+    preco:"79,90",
+    imagem:"",
+  },
+  {
+    codeID:"livro03",
+    nome: "Livro O Temos do Sábio",
+    preco:"59,90",
+    imagem:"",
+  },
+  {
+    codeID:"livro04",
+    nome: "Livro A Furia ods Reis",
+    preco:"59,90",
+    imagem:"",
+  },
+
+  //colecionaveis
+  {
+    codeID:"colecionaveis01",
+    nome: "Jesus Cristo ",
+    preco:"33,00",
+    imagem:"",
+  },
+  {
+    codeID:"colecionaveis02",
+    nome: "Baphomet",
+    preco:"66,60",
+    imagem:"",
+  },
+  {
+    codeID:"colecionaveis03",
+    nome: "São Jorge",
+    preco:"59,90",
+    imagem:"",
+  },
+  {
+    codeID:"colecionaveis04",
+    nome: "Homem de Ferro",
+    preco:"79,90",
+    imagem:"",
+  },
+];
